0163e3b447c9b9346e4d3a152b4d51e32df014fe
MayorFaj <mayorfaj@gmail.com>
2023-11-21 19:55:49 +0000

    file added

---
+++ get_oidc_id.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the cluster_name variable
+export cluster_name=rias-billing-dev
+
+# Use AWS CLI to describe the EKS cluster and retrieve the OIDC issuer URL
+oidc_id=$(aws eks describe-cluster --name $cluster_name --query "cluster.identity.oidc.issuer" --output text | cut -d '/' -f 5)
+
+# Print the OIDC ID
+echo $oidc_id
